d26220084c8ce01c5dbe1e7a91fd776145800359
martin-garcia-blanco <programadopormartin@gmail.com>
2021-05-20 10:50:55 +0200

    Implement Recoil

---
+++ src/componentes/AddItemToList/AddItemToList.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../../Recoil/todoListState";
+
+const AddItemToList = () => {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useRecoilState(todoListState);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const addItemToList = (e) => {
+    e.preventDefault();
+    setItems([...items, inputText]);
+    setInputText("");
+  };
+
+  return (
+    <form onSubmit={addItemToList}>
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default AddItemToList;
