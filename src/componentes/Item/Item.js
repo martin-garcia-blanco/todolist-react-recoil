87b6fa6c2e3a5e063ac6b634f06bfb7418b9d273
martin-garcia-blanco <programadopormartin@gmail.com>
2021-05-20 11:52:42 +0200

    Add capability to remove items

--- src/componentes/Item/Item.js
+++ src/componentes/Item/Item.js
@@ -1,10 +1,19 @@
 import "./item.css";
+import { todoListState } from "../../Recoil/todoListState";
+import { useRecoilState } from "recoil";
 
 const Item = ({ value, id }) => {
+  const [list, setList] = useRecoilState(todoListState);
+
+  const handleRemoveItem = () => {
+    const newList = list.filter((item, index) => id !== index);
+    setList(newList);
+  };
+
   return (
     <div className="item">
       <p>{value}</p>
-      <button>Remove</button>
+      <button onClick={handleRemoveItem}>Remove</button>
     </div>
   );
 };
